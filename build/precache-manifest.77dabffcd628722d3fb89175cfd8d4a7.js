9e81bee816feecec4dceb74c25760693eb9e525d
Thiago Rodrigues <ithiagorodriguessilva@gmail.com>
2021-07-26 12:17:03 -0300

    Teste

---
+++ build/precache-manifest.77dabffcd628722d3fb89175cfd8d4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad4f13b6b69e18566626e3d1b661af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df028328390bb2650661",
+    "url": "/static/css/main.a5670bc0.chunk.css"
+  },
+  {
+    "revision": "1224bcb7cda56b5245b4",
+    "url": "/static/js/2.a5a6c259.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a5a6c259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df028328390bb2650661",
+    "url": "/static/js/main.d194cec2.chunk.js"
+  },
+  {
+    "revision": "8700ffd79a9f229adde3",
+    "url": "/static/js/runtime-main.2ee8a694.js"
+  }
+]);
